a873c2caf127c4f4a84e79bfc4fab472416487e0
yoana <yewonyoana@gmail.com>
2024-03-10 04:39:35 +0900

    Updated File

--- src/models/Video.js
+++ src/models/Video.js
@@ -5,6 +5,7 @@ import mongoose from "mongoose";
 const videoSchema = new mongoose.Schema({
 	title: { type: String, required: true, trim: true, maxLength: 20 },
 	fileUrl: { type: String, required: true },
+	thumbUrl: { type: String, required: true },
 	description: { type: String, required: true, trim: true },
 	createdAt: { type: Date, default: Date.now, required: true },
 	hashtags: [{ type: String, required: true, trim: true }],
