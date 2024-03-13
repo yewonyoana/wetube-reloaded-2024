af9985941b76f54a36a94a7bbd307412f37f8ceb
yoana <yewonyoana@gmail.com>
2024-03-14 01:59:31 +0900

    Updated File

--- src/middleware.js
+++ src/middleware.js
@@ -9,34 +9,34 @@ const s3 = new aws.S3({
 	},
 });
 
-const multerUploader = multerS3({
+const s3ImageUploader = multerS3({
 	s3: s3,
-	bucket: "wetube-reloaded-2024",
+	bucket: "wetube-reloaded-2024/images",
+	acl: "public-read",
+});
+
+const s3VideoUploader = multerS3({
+	s3: s3,
+	bucket: "wetube-reloaded-2024/videos",
 	acl: "public-read",
 });
 
 export const localsMiddleware = (req, res, next) => {
-	// logging in the user by adding info into req.session object
-	//to get if user is logged in (true) or not (false)
-	// locals is an object that can be accessed by the pug templates without having to import and export
 	res.locals.loggedIn = Boolean(req.session.loggedIn);
 	res.locals.siteName = "Wetube";
-	// sharing logged in user with base.pug
 	res.locals.loggedInUser = req.session.user || {};
 	next();
 };
 
-// if user is loggedIn, continue the request or redirect to login
 export const protectorMiddleware = (req, res, next) => {
 	if (req.session.loggedIn) {
-		next();
+		return next();
 	} else {
-		req.flash("error", "Not Authorized");
+		req.flash("error", "Log In First");
 		return res.redirect("/login");
 	}
 };
 
-// if user isn't loggedIn, continue with request or redirect to homepage
 export const publicOnlyMiddleware = (req, res, next) => {
 	if (!req.session.loggedIn) {
 		return next();
@@ -46,18 +46,13 @@ export const publicOnlyMiddleware = (req, res, next) => {
 	}
 };
 
-// middleware to upload files from users to "uploads"
 export const avatarUpload = multer({
-	dest: "uploads/avatars/",
-	limits: {
-		fileSize: 3000000,
-	},
-	storage: multerUploader,
+	dest: "uploads/avatars",
+	limits: { fileSize: 3000000 },
+	storage: s3ImageUploader,
 });
 export const videoUpload = multer({
-	dest: "uploads/videos/",
-	limits: {
-		fileSize: 10000000,
-	},
-	storage: multerUploader,
+	dest: "uploads/videos",
+	limits: { fileSize: 10000000 },
+	storage: s3VideoUploader,
 });
