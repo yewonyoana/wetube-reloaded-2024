7b9c0d3e1ffcd0f43e2c84281ba7a193b6467832
yoana <yewonyoana@gmail.com>
2024-03-14 13:47:37 +0900

    Updated File

--- src/db.js
+++ src/db.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
 
 mongoose.connect(process.env.DB_URL, {
-	// useNewUrlParser: true,
-	// useUnifiedTopology: true,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useFindAndModify: false,
+	useCreateIndex: true,
 });
 
 const db = mongoose.connection;
