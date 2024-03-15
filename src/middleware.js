c8a85bae08cc81c025939bae27cad2f0985ade26
yoana <yewonyoana@gmail.com>
2024-03-16 01:58:11 +0900

    Updated

--- src/middleware.js
+++ src/middleware.js
@@ -3,6 +3,7 @@ import multerS3 from "multer-s3";
 import { S3Client } from "@aws-sdk/client-s3";
 
 const s3 = new S3Client({
+	region: "ap-northeast-2",
 	credentials: {
 		accessKeyId: process.env.AWS_ID,
 		secretAccessKey: process.env.AWS_SECRET,
